12a5b2d19e541b049ea21b25c7ea65c9bac50442
JatinTehri <jatintehri1974@gmail.com>
2025-04-29 11:09:06 +0530

    Added HTML Questions JS file

---
+++ question1.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+    numb: 1,
+    question: "HTML stands for?",
+    answer: "HyperText Markup Language",
+    options: [
+      "HighText Machine Language",
+      "HighText Machine Language",
+      "HyperText Markup Language",
+      "None of these"
+    ]
+  },
+    {
+    numb: 2,
+    question: " The correct sequence of HTML tags for starting a webpage is?",
+    answer: "HTML, Head, Title, Body",
+    options: [
+      "HTML, Body, Title, Head",
+      "HTML, Head, Title, Body",
+      "HTML, Body, Head, Title",
+      "Head, Title, HTML, Body"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the effect of the <b> tag?",
+    answer: "It converts the text within it to bold font.",
+    options: [
+      "It converts the text within it to bold font.",
+      "It is used to write black-colored font.",
+      "It is used to change the font size.",
+      "None of these"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the function of the HTML style attribute?",
+    answer: "It is used to add styles to an HTML element.",
+    options: [
+      "It is used to identify specific styles.",
+      "It is used to add styles to an HTML element.",
+      "Both A and B",
+      "None of these"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following things are necessary to create an HTML page?",
+    answer: "Both A and B",
+    options: [
+      "A text editor",
+      "Web Browser",
+      "Both A and B",
+      "None of these"
+    ]
+  },
+  
+];
