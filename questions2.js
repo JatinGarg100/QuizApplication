cf95e467c28d71eb1f58e66a8327d3d10cde5f14
JatinTehri <jatintehri1974@gmail.com>
2025-04-29 11:17:14 +0530

    Added CSS Questions JS file

---
+++ questions2.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheets",
+    options: [
+      "Cascade Style Sheets",
+      "Color and Style Sheets",
+      "Cascading Style Sheets",
+      "None of these"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What type of CSS is generally recommended for designing large web pages?",
+    answer: "External",
+    options: [
+      "Inline",
+      "Internal",
+      "External",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "How can we select an element with a specific ID in CSS?",
+    answer: "#",
+    options: [
+      "#",
+      ",",
+      "^",
+      "?"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Can negative values be allowed in padding property?",
+    answer: "No",
+    options: [
+      "Yes",
+      "No",
+      "Depends on Property",
+      "None of these"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following is the correct way to select all h1 headers in a div element?",
+    answer: "div-h1",
+    options: [
+      "div h1",
+      "div-h1",
+      "h1",
+      "None of these"
+    ]
+  },
+  
+];
