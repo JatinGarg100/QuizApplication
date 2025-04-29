0545b93f704ee72bc7e83bf193d539090360ded5
JatinTehri <jatintehri1974@gmail.com>
2025-04-29 11:19:04 +0530

    Added Javascript Questions JS file

---
+++ questions3.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following is the correct syntax to declare a variable in JavaScript?",
+    answer: "int x = 10;",
+    options: [
+      "let 1x = 10;",
+      "var x = 10;",
+      "variable x = 10;",
+      "int x = 10;"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following is a falsy value in JavaScript?",
+    answer: "0",
+    options: [
+      "False",
+      "[]",
+      "0",
+      "{}"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the output of the following code?console.log(2 + '2');",
+    answer: "'22'",
+    options: [
+      "4",
+      "'22'",
+      "undefined",
+      "'4'"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following is not a JavaScript data type?",
+    answer: "Character",
+    options: [
+      "String",
+      "Object",
+      "Null",
+      "Character"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which statement is used to stop a loop in JavaScript?",
+    answer: "Break",
+    options: [
+      "Stop",
+      "Halt",
+      "Break",
+      "Exit"
+    ]
+  },
+
+];
