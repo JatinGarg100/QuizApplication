5981f9fd60cb2789a1638df90b5aa84972f918c5
JatinTehri <jatintehri1974@gmail.com>
2025-04-29 11:12:09 +0530

    Added Sports Questions JS file

---
+++ question5.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Where was the 8th edition of the Future Investment Initiative organized?",
+    answer: "Riyadh",
+    options: [
+      "Kuwait",
+      "Abu Dhabi",
+      "Riyadh",
+      "Tehran"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which day is observed as National Unity Day?",
+    answer: "31 October",
+    options: [
+      "29 October",
+      "30 October",
+      "31 october",
+      "27 October"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which country discovered the Saturn-sized exoplanet TOI-6651b?",
+    answer:"India",
+    options: [
+      "India",
+      "China",
+      "Japan",
+      "Russia"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which Indian city set a Guinness World Record by lighting over 2.5 million diyas during Deepotsav 2024?",
+    answer: "Ayodhya",
+    options: [
+      "Varanasi",
+      "Ayodhya",
+      "Mathura",
+      "Haridwar"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which state government launched the 'Elevate 2024' scheme for early-stage startups?",
+    answer: "Karnatka",
+    options: [
+      "Rajasthan",
+      "Maharashtra",
+      "Jharkhand",
+      "Karnatka"
+    ]
+  },
+];
