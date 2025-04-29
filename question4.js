952fe380f5c52427951ec35227d823bb94c46a20
JatinTehri <jatintehri1974@gmail.com>
2025-04-29 11:10:49 +0530

    Added Python Questions JS file

---
+++ question4.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What is the correct file extension for Python files?",
+    answer: ".py",
+    options: [
+      ".py",
+      ".p",
+      ".pt",
+      ".python"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following is used to define a block of code in Python?",
+    answer: " Indentation",
+    options: [
+      "Curly Braces{}",
+      " Indentation",
+      "Parentheses()",
+      "Square brackets[]"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following is not a Python data type?",
+    answer: "Array",
+    options: [
+      "List",
+      "Tuple",
+      "Array",
+      "Dictionary"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the data type of 5j in Python?",
+    answer: "Complex",
+    options: [
+      "Integer",
+      "Float",
+      "Complex",
+      "String"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What will be the output of the following code?",
+    answer: "4",
+    options: [
+      "3",
+      "4",
+      "5",
+      "Error"
+    ]
+  },
+
+];
