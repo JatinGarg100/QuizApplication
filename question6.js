657557abbca41073de70f294f51992fea60dc8f8
JatinTehri <jatintehri1974@gmail.com>
2025-04-29 11:14:15 +0530

    Added Gk Questions JS file

---
+++ question6.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "How many medals India got in Tokyo Olympics 2020?",
+    answer: "7",
+    options: [
+      "5",
+      "7",
+      "10",
+      "15"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which team won ICC Men's T20 world cup 2024?",
+    answer: "India",
+    options: [
+      "India",
+      "England",
+      "South Africa",
+      "Australia"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Manu Bhaker won 2 medals in Paris Olympics 2024 in which sport?",
+    answer: "Shooting",
+    options: [
+      "Shooting",
+      "Archery",
+      "Swimming",
+      "Athletics"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Lakhya Sen represent India in which sport?",
+    answer: "Badminton",
+    options: [
+      "Badminton",
+      "Cricket",
+      "Football",
+      "Tennis"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Who is the current captain of Indian Hockey team?",
+    answer: "Harmanpreet Singh",
+    options: [
+      "Harmanpreet Singh",
+      "Amit Rohidas",
+      "Jarmanpreet Singh",
+      "Hardik"
+    ]
+  },
+];
